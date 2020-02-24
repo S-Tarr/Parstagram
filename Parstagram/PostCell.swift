a116d4d8055494ced4852afecd366a5a60788362
S-Tarr <tarrs@purdue.edu>
2020-02-23 23:06:57 -0500

    asdf

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCellTableViewCell.swift
+//  Parstagram
+//
+//  Created by Sam on 2/23/20.
+//  Copyright © 2020 tarrGames(). All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
